80250292019d109449ea9d2953c62432294e4bc7
Marcus Meekins <meeks_mha@yahoo.com>
2022-05-31 11:42:01 -0400

    Connected my database

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ console.log('Server test!')
 
 
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the pokemon app!');
+})
 
 
 
-
-
-
+app.get('/pokemons', (req, res) => {
+    res.send(pokemons);
+})
+//app.set(viewengine,View.ejs);
+//app.set()
 
 //listen out for the server
 app.listen(PORT,()=>{
